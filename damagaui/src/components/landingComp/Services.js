c2e251ccc8a0d439895a7a5f33435184998a5dbf
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-11-06 22:06:48 -0500

    adjusting feature page for specific content

--- damagaui/src/components/landingComp/Services.js
+++ damagaui/src/components/landingComp/Services.js
@@ -7,27 +7,13 @@ export default function Services() {
     <div className="service-area theme-2 padding-80">
       <div className="container">
         <div className="row">
-          <div
-            className="col-lg-8 mx-md-auto text-center wow fadeInDown"
-            data-wow-duration="2.5s"
-          >
-            <div className="section-title">
-              <h3>App Overview</h3>
-              <p>
-                Dagama gives you the capability to search for nearby outings. It
-                alerts you when events that you've been to in the past are near.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="row">
           <div className="col-lg-3 col-md-6">
             <div className="single-services">
               <div className="service-icon">
                 {" "}
                 <i className="icofont icofont-tools-alt-2" />
               </div>
-              <h4>Easy To Use</h4>
+              <h4>Navigate</h4>
               <p>
                 Dagama is specifically designed to be simple to use. We've kept
                 in mind clarity and convenience for all users.{" "}
@@ -40,7 +26,7 @@ export default function Services() {
                 {" "}
                 <i className="icofont icofont-rocket-alt-1" />
               </div>
-              <h4>Fast Performance</h4>
+              <h4>Connect</h4>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing eiusmod
                 tempor incididunt ut labore et dolore magna aliqua.
@@ -53,7 +39,7 @@ export default function Services() {
                 {" "}
                 <i className="icofont icofont-diamond" />
               </div>
-              <h4>Clean Design</h4>
+              <h4>Explore</h4>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing eiusmod
                 tempor incididunt ut labore et dolore magna aliqua.
@@ -66,7 +52,7 @@ export default function Services() {
                 {" "}
                 <i className="icofont icofont-users" />
               </div>
-              <h4>24/7 Live Support</h4>
+              <h4>Share</h4>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing eiusmod
                 tempor incididunt ut labore et dolore magna aliqua.
