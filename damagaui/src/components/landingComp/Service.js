99ab5144ac885be773929fb3a408da3d52f322db
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-11-11 20:29:00 -0500

    Seperated Service into own component

---
+++ damagaui/src/components/landingComp/Service.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const objProp = {};
+
+objProp.name = "Reggie";
+objProp.description = "Reggie lets his hair grow";
+
+export default function Service(props) {
+  return (
+    <div className="single-services">
+      <div className="service-icon">
+        <i className="icofont icofont-tools-alt-2" />
+      </div>
+      <h4>{props.feature.name}</h4>
+      <p>{props.feature.description}</p>
+    </div>
+  );
+}
--- damagaui/src/components/landingComp/Services.js
+++ damagaui/src/components/landingComp/Services.js
@@ -1,5 +1,27 @@
 import React from "react";
 import "../../sass/layout/_services.scss";
+import Service from "./Service";
+
+const featured = [
+  {
+    icon: "",
+    name: "Navigate",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum."
+  },
+  {
+    icon: "",
+    name: "Explore",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum."
+  },
+  {
+    icon: "",
+    name: "Share",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum."
+  }
+];
 
 export default function Services() {
   return (
@@ -8,61 +30,14 @@ export default function Services() {
       <div className="container">
         <div className="row">
           <div className="col-lg-3 col-md-6">
-            <div className="single-services">
-              <div className="service-icon">
-                {" "}
-                <i className="icofont icofont-tools-alt-2" />
-              </div>
-              <h4>Navigate</h4>
-              <p>
-                Dagama is specifically designed to be simple to use. We've kept
-                in mind clarity and convenience for all users.{" "}
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6">
-            <div className="single-services">
-              <div className="service-icon">
-                {" "}
-                <i className="icofont icofont-rocket-alt-1" />
-              </div>
-              <h4>Connect</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6">
-            <div className="single-services">
-              <div className="service-icon">
-                {" "}
-                <i className="icofont icofont-diamond" />
-              </div>
-              <h4>Explore</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6">
-            <div className="single-services">
-              <div className="service-icon">
-                {" "}
-                <i className="icofont icofont-users" />
-              </div>
-              <h4>Share</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
+            {featured.map(feature => (
+              <Service feature={feature} />
+            ))}
           </div>
         </div>
+
         <div className="row">
           <div className="col-lg-12 text-center">
-            {" "}
             <a href="#" className="store-btn">
               <i className="icofont icofont-brand-apple" /> App Store
             </a>
@@ -73,6 +48,6 @@ export default function Services() {
         </div>
       </div>
     </div>
-    // <!--================Services Area End==============-->
+    /* // <--================Services Area End==============--> */
   );
 }
